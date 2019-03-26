f916d7cd498061187524df11f705da571a434c1d
davidauza.engineer <davidauza01@gmail.com>
2019-03-25 21:52:58 -0500

    Initial commit.

---
+++ app/src/main/java/engineer/davidauza/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package engineer.davidauza.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textView = new TextView(this);
+        textView.setText("Wow!");
+
+        setContentView(textView);
+    }
+}
